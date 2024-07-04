59078aa0618336a7ca203088c066d7f26cba07f8
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-07-04 17:45:40 +0200

    add: updated menu'

--- src/main/java/org/gestore/eventi/java/milestone/Main.java
+++ src/main/java/org/gestore/eventi/java/milestone/Main.java
@@ -1,7 +1,7 @@
 package org.gestore.eventi.java.milestone;
 
 import java.util.Calendar;
-import java.util.Date;
+
 import java.util.GregorianCalendar;
 import java.util.Locale;
 import java.util.Scanner;
@@ -11,7 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 
-		Evento e = new Evento("The Avengers", new GregorianCalendar(2024, 7, 4), 20);
+		Evento e = new Evento("The Avengers", new GregorianCalendar(2024, 6, 4), 20);
 		
 		dataTodayBeautifier();
 		int chooseInput;
@@ -21,47 +21,97 @@ public class Main {
 			chooseInput = choose.nextInt();
 			
 			switch(chooseInput) {
-			case 1:
-				Scanner inputBooked = new Scanner(System.in);
-				System.out.println("- INSERISCI QUANTE PRENOTAZIONI VUORI FARE: ");
-				int seatBooked = inputBooked.nextInt();
-				if(e.checkEvent()) {
-					System.out.println("- L'EVENTO DISPONIBILE");
-					for(int i = 0; i < seatBooked; i++)
-						e.prenota();
-					System.out.println("- POSTI DISPONIBILI ---> " + e.seatsAvailable());
-					System.out.println("- POSTI TOTALI ---> " + e.getSeatsTotal());
-					System.out.println("- POSTI PRENOTATI ---> " + e.getSeatsTotalBooked());
-				}
-				else {
-					System.out.println("- L'EVENTO NON E' PIU' DISPONIBILE");
-				}
-				break;
-				
-			case 2:
-				Scanner inputCancel = new Scanner(System.in);
-				System.out.println("- INSERISCI QUANTE PRENOTAZIONI VUORI CANCELLARE: ");
-				int seatCancel = inputCancel.nextInt();
-				if(e.checkEvent()) {
-					System.out.println("- L'EVENTO DISPONIBILE");
-					for(int i = 0; i < seatCancel; i++)
-						e.disdici();
-					System.out.println("- POSTI DISPONIBILI ---> " + e.seatsAvailable());
-					System.out.println("- POSTI TOTALI ---> " + e.getSeatsTotal());
-					System.out.println("- POSTI PRENOTATI ---> " + e.getSeatsTotalBooked());
-				}
-				else {
-					System.out.println("- L'EVENTO NON E' PIU' DISPONIBILE");
-				}
-				break;
-				
-			case 3:  
-				System.out.println("- ARRIVEDERCI");
-				break;
-				
-			 default:
-				 System.out.println("- INSERISCI UNA SCELTA VALIDA!!!");
-				 break;
+				case 1:
+					//CONTROLLO DATA EVENTO;
+					if(!e.toCheckDate()) {
+						System.out.println(e.toString());
+						System.out.println("- L'EVENTO DISPONIBILE");
+						
+						//CONTROLLO POSTI DISPONIBILI;
+						if(!e.toCheckSeatsAvailable()) {
+							System.out.println("- POSTI DISPONIBILI ---> " + e.seatsAvailableBooked() + "\n");
+							
+							Scanner inputBooked = new Scanner(System.in);
+							System.out.println("- INSERISCI QUANTE PRENOTAZIONI VUORI FARE: ");
+							int seatBooked = inputBooked.nextInt();
+							
+							//CONTROLLO SE LE PRENOTAZIONI NON SUPERINO IL NUMERO DEI POSTI RIMANENTI;
+							if(seatBooked <= e.seatsAvailableBooked() && seatBooked > -1) {
+							
+								for(int i = 0; i < seatBooked; i++) {
+									e.prenota();
+									e.seatsAvailableBooked();
+								}
+								System.out.println("- AGGIORNAMENTO EVENTO...\n");
+							}
+							else
+								System.out.println("- INSERISCI UN NUMERO DI PRENOTAZIONI VALIDO \n");
+							
+							System.out.println(e.toString());
+							System.out.println("- POSTI DISPONIBILI ---> " + e.seatsAvailableBooked());
+							System.out.println("- POSTI TOTALI ---> " + e.getSeatsTotal());
+							System.out.println("- POSTI PRENOTATI ---> " + e.getSeatsTotalBooked() + "\n");
+						}
+						else
+							System.out.println("- NON CI SONO POSTI DISPONIBILI \n");
+					}
+					else
+						System.out.println("- L'EVENTO FUORI PROGRAMMAZIONE \n");
+					
+					break;
+					
+				case 2:
+					//CONTROLLO DATA EVENTO;
+					if(!e.toCheckDate()) {
+						System.out.println(e.toString());
+						System.out.println("- L'EVENTO DISPONIBILE");
+						
+						//CONTROLLO POSTI DISPONIBILI;
+						if(!e.toCheckSeatsAvailable()) {
+							System.out.println("- POSTI PRENOTATI ---> " + e.getSeatsTotalBooked() + "\n");
+							
+							Scanner inputCancel = new Scanner(System.in);
+							System.out.println("- INSERISCI QUANTE PRENOTAZIONI VUORI CANCELLARE: ");
+							int seatCancel = inputCancel.nextInt();
+							
+							//CONTROLLO SE LE CANCELLAZIONI NON SUPERINO IL NUMERO DEI POSTI DISPONIBILI
+							if(seatCancel <= e.getSeatsTotalBooked() && seatCancel > -1) {
+								
+								//CONTROLLO SE ESISTONO PRENOTAZIONI;
+								if(e.getSeatsTotalBooked() > 0) {
+									for(int i = 0; i < seatCancel; i++) 
+										e.disdici();
+									
+									System.out.println("- AGGIORNAMENTO EVENTO...\n");
+								}
+								else
+									System.out.println("- NON CI SONO PRENOTAZIONI DISPONIBILI \n");
+								
+							}else
+								System.out.println("- INSERISCI UN NUMERO DI CANCELLAZIONE VALIDO \n");
+							
+
+							System.out.println(e.toString());
+							System.out.println("- POSTI DISPONIBILI ---> " + e.seatsAvailableBooked());
+							System.out.println("- POSTI TOTALI ---> " + e.getSeatsTotal());
+							System.out.println("- POSTI PRENOTATI ---> " + e.getSeatsTotalBooked() + "\n");
+						}
+						else
+							System.out.println("- POSTI NON DISPONIBILI \n");
+					}
+					else
+						System.out.println("- L'EVENTO FUORI PROGRAMMAZIONE \n");
+					
+					break;
+					
+					
+				case 3:  
+					System.out.println("- ARRIVEDERCI");
+					break;
+					
+				 default:
+					 System.out.println("- INSERISCI UNA SCELTA VALIDA!!! \n");
+					 break;
 			}
 		}while(chooseInput != 3);
 
