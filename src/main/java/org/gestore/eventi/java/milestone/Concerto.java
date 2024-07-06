b0e11c79ea88d479f450869570140a7b2b38c4e9
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-07-06 18:38:08 +0200

    added: created new Concerto class with a time instance variable and a price instance variable; overridden toString and created a method to process price and date/time

---
+++ src/main/java/org/gestore/eventi/java/milestone/Concerto.java
@@ -0,0 +1,67 @@
+package org.gestore.eventi.java.milestone;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Concerto extends Evento{
+
+	protected Calendar time;
+	protected double price;
+	
+	public Concerto(String title, Calendar date, int seatsTotal, Calendar time, double price) {
+		super(title, date, seatsTotal);
+		time = this.time;
+		price = this.price;
+	}
+
+	// METODI SETTER AND GETTER;
+	public Calendar getTime() {
+		return time;
+	}
+
+	public void setTime(Calendar time) {
+		this.time = time;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+	/*
+	 * METODO CHE FORMATTA IL PREZZO;
+	 */
+	public String priceBeautifier() {
+		return String.format("%.2f", this.price) + "€";
+	}
+	
+	
+	@Override
+	public String dataBeautifier() {
+		
+		int day = this.date.get(Calendar.DATE);
+		String month = this.date.getDisplayName(Calendar.MONTH, Calendar.LONG_FORMAT, Locale.ITALY);
+		int year = this.date.get(Calendar.YEAR);
+		int hour = this.date.get(Calendar.HOUR_OF_DAY);
+		int minute = this.date.get(Calendar.MINUTE);
+		int second = this.date.get(Calendar.SECOND);
+		String dateBeautifier = day + " " + month + " " + year + " - " + hour + ":" + minute + ":" + second;
+		
+		return dateBeautifier;
+
+	}
+	
+	
+	@Override
+	public String toString() {
+		return this.dataBeautifier() + " - " + this.title + " - " + this.priceBeautifier() + "\n";
+	}
+	
+	
+	
+	
+}
